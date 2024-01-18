a04794bb19dd984c9b3b3aac69e3e7479a0a8b40
masud-rana44 <mdmasudrana4488@gmail.com>
2024-01-18 22:49:36 +0600

    Setup supabase & added ui components

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://cacowtdrizerhqppygsn.supabase.co";
+const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
